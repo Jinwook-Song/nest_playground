568cec6e4334f0fee538cd71abfecb40057c826a
jinwook song <wlsdnr129@naver.com>
2025-08-25 23:18:44 +0900

    feat: common module

--- config-module-builder/apps/main/src/main.module.ts
+++ config-module-builder/apps/main/src/main.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
 import { MainController } from './main.controller';
 import { MainService } from './main.service';
+import { CommonModule } from '@app/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    CommonModule.registerAsync({
+      useFactory: (configService: ConfigService) => ({
+        url: configService.getOrThrow('URL_MAIN'),
+      }),
+      inject: [ConfigService],
+    }),
+  ],
   controllers: [MainController],
   providers: [MainService],
 })
--- config-module-builder/libs/common/src/common.service.ts
+++ config-module-builder/libs/common/src/common.service.ts
@@ -1,4 +1,18 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable, OnModuleInit } from '@nestjs/common';
+import { MODULE_OPTIONS_TOKEN } from './common.module-definition';
+import type { CommonModuleOptions } from './common-module-options.interface';
 
 @Injectable()
-export class CommonService {}
+export class CommonService implements OnModuleInit {
+  constructor(
+    @Inject(MODULE_OPTIONS_TOKEN) private readonly options: CommonModuleOptions,
+  ) {}
+
+  onModuleInit() {
+    setInterval(async () => {
+      const res = await fetch(this.options.url);
+      const parsed = await res.text();
+      console.log(parsed);
+    }, 1000);
+  }
+}
