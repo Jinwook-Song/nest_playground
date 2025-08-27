ceb1cb61da8719404c62d4f90551539bbe0d1d42
jinwook song <wlsdnr129@naver.com>
2025-08-27 22:07:08 +0900

    feat(multitenancy): middleware

---
+++ nestjs-multitenancy/src/tenancy/tenancy.service.ts
@@ -0,0 +1,22 @@
+import { readFileSync } from 'fs';
+import { Tenants } from './tenants.inteface';
+import { BadRequestException, Injectable, OnModuleInit } from '@nestjs/common';
+import { join } from 'path';
+
+@Injectable()
+export class TenancyService implements OnModuleInit {
+  private tenants: Tenants;
+
+  onModuleInit() {
+    const tenants: Tenants = JSON.parse(
+      readFileSync(join(__dirname, './tenants.json'), 'utf8'),
+    );
+    this.tenants = tenants;
+  }
+
+  validateTenantId(tenantId: string) {
+    if (!this.tenants[tenantId]) {
+      throw new BadRequestException(`Invalid tenant ID: ${tenantId}`);
+    }
+  }
+}
