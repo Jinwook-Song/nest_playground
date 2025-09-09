2e0efb114f77c8af859e43a6b5880b920b9a5779
jinwook song <wlsdnr129@naver.com>
2025-09-09 23:05:36 +0900

    chatter(frontend): use login

--- chatter-app/frontent/src/components/auth/Login.tsx
+++ chatter-app/frontent/src/components/auth/Login.tsx
@@ -1,16 +1,13 @@
 import { Link } from 'react-router-dom';
 import { Link as MuiLink } from '@mui/material';
 import Auth from './Auth';
+import { useLogin } from '../../hooks/useLogin';
 
 const Login = () => {
-  const handleSubmit = async (credentials: {
-    email: string;
-    password: string;
-  }) => {
-    console.log(credentials);
-  };
+  const { login, error } = useLogin();
+
   return (
-    <Auth submitLabel='Login' onSubmit={handleSubmit}>
+    <Auth submitLabel='Login' onSubmit={login}>
       <Link to='/signup' style={{ textAlign: 'center' }}>
         <MuiLink component={Link} to='/signup'>
           Sign Up &rarr;
