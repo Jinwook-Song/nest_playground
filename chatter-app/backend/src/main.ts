3cf17d1f58829d4058cdd9bfa92f1d0e193f98c8
jinwook song <wlsdnr129@naver.com>
2025-09-24 22:41:51 +0900

    chatter: fix cors

--- chatter-app/backend/src/main.ts
+++ chatter-app/backend/src/main.ts
@@ -8,7 +8,14 @@ import cookieParser from 'cookie-parser';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { bufferLogs: true });
 
-  app.enableCors();
+  app.enableCors({
+    origin: [
+      'https://chatter-frontend.d2ya120aqlr9yf.amplifyapp.com', // 프론트엔드 도메인
+      'http://localhost:3000', // 로컬 개발용
+    ],
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true,
+  });
 
   app.useGlobalPipes(new ValidationPipe());
   app.useLogger(app.get(Logger));
