014cc9fa27f24675c3459aed24b3cbdc9a482cd0
jinwook song <wlsdnr129@naver.com>
2025-10-18 15:30:09 +0900

    feat(hexigonal): get user use case

--- nestjs-hexigonal/src/user/user.module.ts
+++ nestjs-hexigonal/src/user/user.module.ts
@@ -3,11 +3,13 @@ import { CreateUserUseCase } from './application/use-cases/create-user.use-case'
 import { USER_REPOSITORY } from './application/ports/user.repository.port';
 import { InMemoryUserRepository } from './infrastructure/adapters/in-memory-user.repository';
 import { UserController } from './presentation/user.controller';
+import { GetUserUseCase } from './application/use-cases/get-user.use-case';
 
 @Module({
   controllers: [UserController],
   providers: [
     CreateUserUseCase,
+    GetUserUseCase,
     {
       provide: USER_REPOSITORY,
       useClass: InMemoryUserRepository,
