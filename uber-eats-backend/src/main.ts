1b978b31fa76563e134f495763c170388a9ed773
jinwook song <wlsdnr129@naver.com>
2025-10-23 16:11:02 +0900

    uber: init

--- uber-eats-backend/src/main.ts
+++ uber-eats-backend/src/main.ts
@@ -1,14 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { NestFastifyApplication } from '@nestjs/platform-fastify';
 
 async function bootstrap() {
-  const app = await NestFactory.create<NestFastifyApplication>(AppModule, {
-    logger: ['error', 'warn', 'log', 'verbose', 'debug'],
-  });
-
-  const port = process.env.PORT ?? 3000;
-  await app.listen(port);
-  console.log(`Server is running on port ${port}`);
+  const app = await NestFactory.create(AppModule);
+  await app.listen(3000);
 }
 bootstrap();
