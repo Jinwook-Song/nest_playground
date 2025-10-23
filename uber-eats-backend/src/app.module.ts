8257a1035ab015f26ceb82a7806541492a2c0078
jinwook song <wlsdnr129@naver.com>
2025-10-24 01:35:58 +0900

    uber: validate with joi

--- uber-eats-backend/src/app.module.ts
+++ uber-eats-backend/src/app.module.ts
@@ -3,6 +3,7 @@ import { GraphQLModule } from '@nestjs/graphql';
 import { ShopsModule } from './shops/shops.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule } from '@nestjs/config';
+import * as Joi from 'joi';
 
 @Module({
   imports: [
@@ -12,13 +13,17 @@ import { ConfigModule } from '@nestjs/config';
         process.env.NODE_ENV === 'development'
           ? '.env.development'
           : '.env.production',
+      validationSchema: Joi.object({
+        DATABASE_PATH: Joi.string().required(),
+        NODE_ENV: Joi.string().valid('development', 'production').required(),
+      }),
     }),
     TypeOrmModule.forRoot({
       type: 'sqlite',
-      database: 'db.sqlite',
+      database: process.env.DATABASE_PATH || 'db.sqlite',
       entities: [__dirname + '/**/*.entity{.ts,.js}'],
-      synchronize: true,
-      logging: true,
+      synchronize: process.env.NODE_ENV !== 'production',
+      logging: process.env.NODE_ENV === 'development',
     }),
     GraphQLModule.forRoot({
       autoSchemaFile: true,
